8202972b5ae09234828dbd23c0a3ab913f2801d7
redDeadth <mimamamemima335@gmail.com>
2025-09-28 23:49:44 -0500

    Hueso

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,12 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
-import db from '@astrojs/db';
-
 import vercel from '@astrojs/vercel';
 
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
   output: 'server',
-  integrations: [db()],
   adapter: vercel({
     webAnalytics: {
       enabled: true
